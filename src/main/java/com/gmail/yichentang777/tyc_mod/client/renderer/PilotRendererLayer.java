a0b4e0f16182fb84b8d634aa152f53c7a4788472
yichen tang <ge92dom@mytum.de>
2024-12-27 11:22:59 +0100

    Custom render layer.
    -render the pilot according to aircraft orientation

--- src/main/java/com/gmail/yichentang777/tyc_mod/client/renderer/PilotRendererLayer.java
+++ src/main/java/com/gmail/yichentang777/tyc_mod/client/renderer/PilotRendererLayer.java
@@ -2,42 +2,35 @@ package com.gmail.yichentang777.tyc_mod.client.renderer;
 
 import com.gmail.yichentang777.tyc_mod.entities.AircraftEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.logging.LogUtils;
 import com.mojang.math.Axis;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
-import net.minecraft.client.renderer.entity.layers.ElytraLayer;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
-import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
 
 public class PilotRendererLayer  extends RenderLayer<AbstractClientPlayer,PlayerModel<AbstractClientPlayer>>{
 
-    private static final Logger LOGGER = LogUtils.getLogger();
     public PilotRendererLayer(RenderLayerParent<AbstractClientPlayer,PlayerModel<AbstractClientPlayer>> parent) {
         super(parent);
     }
     @Override
-    public void render(PoseStack poseStack, MultiBufferSource buffer, int packedLight,
+    public void render(@NotNull PoseStack poseStack, @NotNull MultiBufferSource buffer, int packedLight,
                        @NotNull AbstractClientPlayer player, float limbSwing, float limbSwingAmount,
                        float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         if (player.getVehicle() instanceof AircraftEntity aircraft) {
-            double[] eulerangles = aircraft.recoverRotationsFromCoordinate();
-            poseStack.pushPose();
+            //retrieve Euler Angles dynamically from aircraft being ridden. 
+            double[] euler_angles = aircraft.recoverRotationsFromCoordinate();
+            //Reset player body rotation to prevent steering the body by mouse cursor.
             player.setYBodyRot(0.0f);
-
-
-            poseStack.translate(0.0D, 0.0D, 0.0D);
-            poseStack.mulPose(Axis.YP.rotation(-(float) eulerangles[1])); // Yaw
-            poseStack.mulPose(Axis.XP.rotation((float) eulerangles[0])); // Pitch
-            poseStack.mulPose(Axis.YP.rotation(-(float) eulerangles[2])); // Roll
+            //Apply Euler Angles to compute pilot orientation. Ensure compatible with aircraft.
+            poseStack.pushPose();
+            poseStack.mulPose(Axis.YP.rotation(-(float) euler_angles[1]));
+            poseStack.mulPose(Axis.XP.rotation((float) euler_angles[0]));
+            poseStack.mulPose(Axis.YP.rotation(-(float) euler_angles[2]));
             this.getParentModel().renderToBuffer(poseStack,
                     buffer.getBuffer(RenderType.entityTranslucent(player.getSkin().texture())),
                     packedLight, OverlayTexture.NO_OVERLAY,
