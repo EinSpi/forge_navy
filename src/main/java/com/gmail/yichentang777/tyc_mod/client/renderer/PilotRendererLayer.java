15ec59743a2b715369f3d97ee060f4a4cdde6e88
yichen tang <ge92dom@mytum.de>
2025-01-20 09:39:17 +0100

    Client-Server Synchronization
    -keeps track of the aircraft's dynamic field "ref" as local copy of its ongoing reference frame.
    -if the ac is controlled by local player, calculate render parameters and send them together with "ref" itself to the server.
    -otherwise trust the render paras in "ref" and applies render accordingly, no calculations are performed.

--- src/main/java/com/gmail/yichentang777/tyc_mod/client/renderer/PilotRendererLayer.java
+++ src/main/java/com/gmail/yichentang777/tyc_mod/client/renderer/PilotRendererLayer.java
@@ -1,6 +1,7 @@
 package com.gmail.yichentang777.tyc_mod.client.renderer;
 
 import com.gmail.yichentang777.tyc_mod.entities.AircraftEntity;
+import com.gmail.yichentang777.tyc_mod.utils.AircraftLocalRef;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import net.minecraft.client.model.PlayerModel;
@@ -23,15 +24,17 @@ public class PilotRendererLayer extends RenderLayer<AbstractClientPlayer, Player
                        @NotNull AbstractClientPlayer player, float limbSwing, float limbSwingAmount,
                        float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
         if (player.getVehicle() instanceof AircraftEntity aircraft) {
-            //retrieve Euler Angles dynamically from aircraft being ridden. 
-            double[] euler_angles = aircraft.recoverRotationsFromCoordinate();
+            //retrieve Euler Angles dynamically from aircraft being ridden.
             //Reset player body rotation to prevent steering the body by mouse cursor.
             player.setYBodyRot(0.0f);
             //Apply Euler Angles to compute pilot orientation. Ensure compatible with aircraft.
             poseStack.pushPose();
-            poseStack.mulPose(Axis.YP.rotation(-(float) euler_angles[1]));
-            poseStack.mulPose(Axis.XP.rotation((float) euler_angles[0]));
-            poseStack.mulPose(Axis.YP.rotation(-(float) euler_angles[2]));
+            if (aircraft.isControlledByLocalInstance()){
+                aircraft.ref.recoverRotationsFromCoordinate();
+            }
+            poseStack.mulPose(Axis.YP.rotation(-(float) aircraft.ref.getRenderAngle2()));
+            poseStack.mulPose(Axis.XP.rotation((float) aircraft.ref.getRenderAngle1()));
+            poseStack.mulPose(Axis.YP.rotation(-(float) aircraft.ref.getRenderAngle3()));
             this.getParentModel().renderToBuffer(poseStack,
                     buffer.getBuffer(RenderType.entityTranslucent(player.getSkin().texture())),
                     packedLight, OverlayTexture.NO_OVERLAY,
---
+++ src/main/java/com/gmail/yichentang777/tyc_mod/utils/AircraftLocalRef.java
@@ -0,0 +1,132 @@
+package com.gmail.yichentang777.tyc_mod.utils;
+
+import net.minecraft.world.phys.Vec3;
+
+public class AircraftLocalRef {
+
+    private Vec3 mainX;
+    private Vec3 mainY;
+    private Vec3 mainZ;
+    private double renderAngle1;
+    private double renderAngle2;
+    private double renderAngle3;
+
+    public AircraftLocalRef(Vec3 mainX, Vec3 mainY, Vec3 mainZ, 
+                            double renderAngle1, double renderAngle2, double renderAngle3) {
+        this.mainX = mainX;
+        this.mainY = mainY;
+        this.mainZ = mainZ;
+        this.renderAngle1 = renderAngle1;
+        this.renderAngle2 = renderAngle2;
+        this.renderAngle3 = renderAngle3;
+    }
+
+    public AircraftLocalRef(Vec3 mainX, Vec3 mainY,Vec3 mainZ){
+        this.mainX = mainX;
+        this.mainY = mainY;
+        this.mainZ = mainZ;
+        recoverRotationsFromCoordinate();
+    }
+
+    public Vec3 getMainX() {
+        return mainX;
+    }
+    public Vec3 getMainY() {
+        return mainY;
+    }
+    public Vec3 getMainZ() {
+        return mainZ;
+    }
+
+    public double getRenderAngle1() {
+        return renderAngle1;
+    }
+    public double getRenderAngle2() {
+        return renderAngle2;
+    }
+    public double getRenderAngle3() {
+        return renderAngle3;
+    }
+
+    public void setMainX(Vec3 mainX) {
+        this.mainX = mainX;
+    }
+    public void setMainY(Vec3 mainY) {
+        this.mainY = mainY;
+    }
+    public void setMainZ(Vec3 mainZ) {
+        this.mainZ = mainZ;
+    }
+    
+    public void rotateAround(boolean direction, double aX, double aY, double aZ, double sinSensitivity, double cosSensitivity) {
+        double qx = direction? aX * sinSensitivity:aX*(-sinSensitivity);
+        double qy = direction? aY * sinSensitivity:aY*(-sinSensitivity);
+        double qz = direction? aZ * sinSensitivity:aZ*(-sinSensitivity);
+        double qw = cosSensitivity;
+        double w2 = qw * qw;
+        double x2 = qx * qx;
+        double y2 = qy * qy;
+        double z2 = qz * qz;
+        double zw = qz * qw;
+        double xy = qx * qy;
+        double xz = qx * qz;
+        double yw = qy * qw;
+        double yz = qy * qz;
+        double xw = qx * qw;
+        double a = (w2 + x2 - z2 - y2);
+        double b = (-zw + xy - zw + xy);
+        double c = (yw + xz + xz + yw);
+        double d = (xy + zw + zw + xy);
+        double e = (y2 - z2 + w2 - x2);
+        double f = (yz + yz - xw - xw);
+        double g = (xz - yw + xz - yw);
+        double h = (yz + yz + xw + xw);
+        double i = (z2 - y2 - x2 + w2);
+
+        double x = mainX.x;
+        double y = mainX.y;
+        double z = mainX.z;
+        double x_end = a * x + b * y + c * z;
+        double y_end = d * x + e * y + f * z;
+        double z_end = g * x + h * y + i * z;
+        this.mainX = new Vec3(x_end, y_end, z_end).normalize();
+
+        x=mainY.x;
+        y=mainY.y;
+        z=mainY.z;
+        x_end = a * x + b * y + c * z;
+        y_end = d * x + e * y + f * z;
+        z_end = g * x + h * y + i * z;
+        this.mainY =new Vec3(x_end, y_end, z_end).normalize();
+
+        x=mainZ.x;
+        y=mainZ.y;
+        z=mainZ.z;
+        x_end = a * x + b * y + c * z;
+        y_end = d * x + e * y + f * z;
+        z_end = g * x + h * y + i * z;
+        this.mainZ =new Vec3(x_end, y_end, z_end).normalize();
+
+
+    }
+
+    public void recoverRotationsFromCoordinate() {
+        double sinbeta = Math.sqrt((mainZ.y * mainZ.y) + (mainX.y * mainX.y));
+        double beta = Math.atan2(sinbeta,mainY.y);
+        if (beta <= 0.017d) {
+            
+            renderAngle1 = 0.0d;
+            renderAngle2 = 0.0d;
+            renderAngle3 = Math.atan2(-mainX.z, mainZ.z);
+        } else if (beta >= 3.124d) {
+            renderAngle1 = Math.PI;
+            renderAngle2 = 0.0d;
+            renderAngle3 = Math.atan2(mainX.z, -mainZ.z);
+        } else {
+            renderAngle1 = beta;
+            renderAngle2 = Math.atan2(mainY.x / sinbeta, mainY.z / sinbeta);
+            renderAngle3 = Math.atan2(mainX.y / sinbeta, -mainZ.y / sinbeta);
+        }
+
+    }
+}
