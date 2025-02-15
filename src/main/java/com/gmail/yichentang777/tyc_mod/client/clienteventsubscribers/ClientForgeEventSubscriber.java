771ede549503d0cec56ed30f38b6f0b411a33200
yichen tang <ge92dom@mytum.de>
2025-02-15 17:23:54 +0100

    Camera Angles adjustment.

--- src/main/java/com/gmail/yichentang777/tyc_mod/client/clienteventsubscribers/ClientForgeEventSubscriber.java
+++ src/main/java/com/gmail/yichentang777/tyc_mod/client/clienteventsubscribers/ClientForgeEventSubscriber.java
@@ -4,23 +4,32 @@ package com.gmail.yichentang777.tyc_mod.client.clienteventsubscribers;
 import com.gmail.yichentang777.tyc_mod.TycMod;
 import com.gmail.yichentang777.tyc_mod.client.keybindings.KeyBindings;
 import com.gmail.yichentang777.tyc_mod.entities.aircrafts.AircraftEntity;
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.CameraType;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.client.event.RenderLivingEvent;
+import net.minecraftforge.client.event.ViewportEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
 
 
 @Mod.EventBusSubscriber(modid = TycMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientForgeEventSubscriber {
-
+    private static final Logger LOGGER = LogUtils.getLogger();
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void AircraftRollHandler(TickEvent.ClientTickEvent event) {
         //Centralized handling aircraft control logics involving customized keymappings.
@@ -80,6 +89,27 @@ public class ClientForgeEventSubscriber {
         }
         keyMapping.setDown(false);
     }
+    @SubscribeEvent
+    public static void onCameraSetup(ViewportEvent.ComputeCameraAngles event) {
+        Minecraft mc = Minecraft.getInstance();
+        Entity cameraEntity = mc.getCameraEntity();
+
+        if (mc.options.getCameraType().isFirstPerson() && cameraEntity instanceof LocalPlayer player) {
+            Entity vehicle = player.getVehicle();
+
+            if (vehicle instanceof AircraftEntity aircraft) {
+
+
+                // Set yaw (left-right rotation)
+                event.setYaw((float)(-aircraft.ref.getRenderAngle2()*(180.0D/Math.PI)));
+                // Set pitch (up-down rotation)
+                event.setPitch((float)(aircraft.ref.getRenderAngle1()*(180.0D/Math.PI)));
+
+
+            }
+        }
+    }
+
 
 
 }
